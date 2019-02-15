a2f9361c4c3c84fd8fd7c0153cf45cd0f4c3625e
Luke Strama <strama@ualberta.ca>
2019-02-15 08:09:18 -0700

    basic tip calc working

--- App.tsx
+++ App.tsx
@@ -1,11 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
-export default class App extends React.Component {
+interface Props {
+
+}
+
+interface State {
+  totalBill: string,
+  tipPercentage: number
+}
+
+export default class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      totalBill: '',
+      tipPercentage: .15
+    }
+  }
+
+  handleInput = (text: string) :void => {
+    this.setState({
+      totalBill: text
+    })
+  }
+
   render() {
+    let tip = '0.00';
+    let tipPercentages = [0.1, 0.15, 0.18, 0.2, 0.3];
+    if (this.state.totalBill) {
+      let tipCalc = (parseFloat(this.state.totalBill) * (this.state.tipPercentage));
+      tip = (Math.round(tipCalc * 100) / 100).toFixed(2);
+    }
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js  start working on your app!</Text>
+        <Text>${tip}</Text>
+        <TextInput 
+          onChangeText={this.handleInput}
+          value={this.state.totalBill}
+          style={styles.input}
+          placeholder="$0.00"
+          keyboardType="number-pad"
+        />
+        <View style={styles.tipButtons}>
+          {tipPercentages.map((percent, index) => (
+            <Button 
+              key={index}
+              title={tipPercentages[index] * 100 + '%'} 
+              onPress={() => this.setState({ tipPercentage: tipPercentages[index]})}
+            />
+          ))
+          }
+        </View>
       </View>
     );
   }
@@ -17,5 +64,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 20
+
   },
+  tipButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    marginTop: 20
+  },
+  input: { 
+    borderWidth: 1,
+    padding: 5,
+    height: 40,
+    width: '100%'
+  }
 });
